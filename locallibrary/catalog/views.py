a711ce64c89d9555fac80a9628e17f9984ff75d9
favour-olumese <olumesefavor@gmail.com>
2022-09-07 14:12:32 +0100

    Worked on the index page.

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
+from .models import Book, Author, BookInstance, Genre
+
 # Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects.
+    num_books = Book.objects.all().count()
+    num_instance = Book.objects.all().count()
+
+    # Available books (status = 'a').
+    num_instance_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instance': num_instance,
+        'num_instance_available': num_instance_available,
+        'num_authors': num_authors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'catalog/index.html', context=context)
