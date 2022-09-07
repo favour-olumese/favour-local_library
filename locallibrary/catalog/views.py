4d7da8c4a50b974f7b87fc8dde3d30e404ce39d9
fav <me@fav.com>
2022-09-07 16:08:48 +0100

    Added the number of genres. To work on the number of Christian genres

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -8,7 +8,7 @@ def index(request):
 
     # Generate counts of some of the main objects.
     num_books = Book.objects.all().count()
-    num_instance = Book.objects.all().count()
+    num_instance = BookInstance.objects.all().count()
 
     # Available books (status = 'a').
     num_instance_available = BookInstance.objects.filter(status__exact='a').count()
@@ -16,11 +16,17 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_genres = Genre.objects.all().count()
+
+    num_christian_genre = Genre.objects.filter(name='Christianity')
+
     context = {
         'num_books': num_books,
         'num_instance': num_instance,
         'num_instance_available': num_instance_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_christian_genre': num_christian_genre,
     }
 
     # Render the HTML template index.html with the data in the context variable
