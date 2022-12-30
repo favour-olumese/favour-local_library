6dd3447909084ab59e266979af0adabb2c77aaea
favour-olumese <olumesefavor@gmail.com>
2022-12-30 22:43:39 +0100

    Worked on local storage saving themes

--- locallibrary/static/js/index.js
+++ locallibrary/static/js/index.js
@@ -1,63 +1,92 @@
 // Changing the text of a button when clicked
-// https://bobbyhadz.com/blog/javascript-change-button-text-on-click#:~:text=To%20change%20the%20text%20of,textContent%20%3D%20%27Button%20clicked%27%20.
 
-const theme_btn = document.getElementById('theme_btn');
+// Accessing theme stored in local storage.
+theme = localStorage.getItem("theme");
 
+if (theme == null)
+{
+    theme = localStorage.setItem("theme", "light");
+}
+
+var theme_btn = document.getElementById('theme_btn');
+
+if (theme == 'dark'){
+    darkMode();
+    document.getElementById('theme_btn').onclick = lightMode;
+    theme_btn.innerHTML = '<i class="fa fa-sun-o" aria-hidden="true"></i>';  
+} else {
+    lightMode();
+    document.getElementById('theme_btn').onclick = darkMode;
+    theme_btn.innerHTML = '<i class="fa fa-moon-o" aria-hidden="true"></i>';
+}
+
+
+// Changing page theme when the change of mode button is clicked.
 theme_btn.addEventListener('click', function handleClick() {
-const initialHTML = '<i class="fa fa-moon-o" aria-hidden="true"></i>';
 
     if (theme_btn.innerHTML == '<i class="fa fa-sun-o" aria-hidden="true"></i>') {
         theme_btn.innerHTML = '<i class="fa fa-moon-o" aria-hidden="true"></i>';
+        document.getElementById('theme_btn').onclick = darkMode;
+        theme = localStorage.setItem("theme", "light");
     } else {
         theme_btn.innerHTML = '<i class="fa fa-sun-o" aria-hidden="true"></i>';
+        document.getElementById('theme_btn').onclick = lightMode;
+        theme = localStorage.setItem("theme", "dark");
     }
 });
 
-// Function for changing the screen mode
-// https://www.geeksforgeeks.org/how-to-make-dark-mode-for-websites-using-html-css-javascript/#:~:text=Steps%20to%20create%20Dark-Mode%20websites%20using%20HTML%2C%20CSS%2C,to%20switch%20between%20dark-mode%20and%20light-mode%20using%20JavaScript
+// Functions for changing the screen theme.
 function darkMode() {
     var element = document.body;
-    element.classList.toggle("dark-mode");
+    element.className = "dark-mode";
+    theme = localStorage.setItem("theme", "dark");
 }
 
-// Index Page
+function lightMode() {
+    var element = document.body;
+    element.className = "light-mode";
+    theme = localStorage.setItem("theme", "light");
+}
 
-let span = document.getElementsByClassName('slider-arrow');
-let product = document.getElementsByClassName('product');
-let product_page = Math.ceil(product.length/4);
-let l = 0;
-let movePer = 25.34;
-let maxMove = 203;
+// INDEX PAGE
+// For controlling carousels
+
+// let span = document.getElementsByClassName('slider-arrow');
+// let product = document.getElementsByClassName('product');
+// let product_page = Math.ceil(product.length/3);
+// let l = 0;
+// let movePer = 50.36;
+// let maxMove = 504;
 
 // Mobile view
-let mobile_view = window.matchMedia("(max-width: 768px)");
-if (mobile_view.matches)
-{
-    movePer = 50.36;
-    maxMove = 504;
-}
+// let mobile_view = window.matchMedia("(max-width: 768px)");
+// if (mobile_view.matches)
+// {
+//     movePer = 50.36;
+//     maxMove = 504;
+// }
 
-let right_mover = ()=> {
-    l = l + movePer;
-    if (product == 1) {l = 0;}
+// let right_mover = ()=> {
+//     l = l + movePer;
+//     if (product == 1) {l = 0;}
 
-    for (const i of product)
-    {
-        if (l > maxMove) {l = l - movePer}
-        i.style.left = '-' + l + '%';
-    }
-}
+//     for (const i of product)
+//     {
+//         if (l > maxMove) {l = l - movePer}
+//         i.style.left = '-' + l + '%';
+//     }
+// }
 
-let left_mover = ()=> {
-    l = l - movePer;
-    if (l <= 0) {l = 0;}
+// let left_mover = ()=> {
+//     l = l - movePer;
+//     if (l <= 0) {l = 0;}
 
-    for (const i of product)
-    {
-        if (product_page > 1)
-        i.style.left = '-' + l + '%';
-    }
-}
+//     for (const i of product)
+//     {
+//         if (product_page > 1)
+//         i.style.left = '-' + l + '%';
+//     }
+// }
 
-span[1].onclick = ()=>{right_mover();}
-span[0].onclick = ()=>{left_mover();}
+// span[1].onclick = ()=>{right_mover();}
+// span[0].onclick = ()=>{left_mover();}
